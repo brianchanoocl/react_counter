14e9ea4ea76831e5eac4917c09b82f9148f80382
brianchanoocl <Delubdi118>
2021-12-14 14:08:23 +0800

    add store, save before remove update

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { UPDATE_SIZE } from "../constants/constants";
 
 function CounterSizeGenerator(props) {
     const [size, setSize] = useState(0);
+    const dispatch = useDispatch();
 
     function handleChangeSize(event) {
         const sizeValue = isNaN(parseInt(event.target.value)) ? 0 : parseInt(event.target.value);
         setSize(sizeValue);
+        dispatch({type: UPDATE_SIZE, payload: sizeValue});
         props.updateSize(sizeValue);
     }
 
