1e9a4a7bb7b3ad30e91b8890663c7c8280381295
brianchanoocl <Delubdi118>
2021-12-14 14:22:15 +0800

    redux update sum

--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -1,26 +1,17 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import CounterGroup from "./CounterGroup";
 import CounterGroupSum from "./CounterGroupSum";
 import CounterSizeGenerator from "./CounterSizeGenerator";
 
 function MultiCounter() {
-    const [sum, setSum] = useState(0);
     const size = useSelector(state => state.size);
-
-    function increase() {
-        setSum(sum + 1);
-    }
-
-    function decrease() {
-        setSum(sum - 1);
-    }
+    const sum = useSelector(state => state.sum);
 
     return(
         <div>
             <CounterSizeGenerator></CounterSizeGenerator>
             <CounterGroupSum sum={sum}></CounterGroupSum>
-            <CounterGroup size={size} increase={increase} decrease={decrease}></CounterGroup>
+            <CounterGroup size={size}></CounterGroup>
         </div>
     );
 }
